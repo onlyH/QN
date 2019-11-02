3fe6264018b9aed7daac608b401740bdb9c559c7
onlyh <736087284@qq.com>
2019-11-02 20:04:46 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,6 @@
   </div>
 </template>
 <style lang="stylus">
-
-
-  /*a*/
-  /*font-weight bold*/
-  /*color #2c3e50*/
-  /*&.router-link-exact-active*/
-  /*color #42b983*/
-
   #app
     min-height: 100%
 </style>
